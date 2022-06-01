e04bf1bb26187567408f9ca76d45445c5af6bccc
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2022-06-01 12:03:04 +0700

    #Add: routing pages dashboard, student, department and faculty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'dashboard', component: AppComponent },
-  { path: 'students', component: AppComponent },
-  { path: 'departments', component: AppComponent },
-  { path: 'faculty', component: AppComponent },
+  {
+    path: '',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then((module) => module.DashboardModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then((module) => module.DashboardModule),
+  },
+  {
+    path: 'students',
+    loadChildren: () => import('./pages/student/student.module').then((module) => module.StudentModule),
+  },
+  {
+    path: 'departments',
+    loadChildren: () => import('./pages/department/department.module').then((module) => module.DepartmentModule),
+  },
+  {
+    path: 'faculty',
+    loadChildren: () => import('./pages/faculty/faculty.module').then((module) => module.FacultyModule),
+  },
 ];
 
 @NgModule({
